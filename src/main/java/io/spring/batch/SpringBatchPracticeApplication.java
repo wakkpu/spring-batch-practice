cadfa195b6fb04a42ec6146cc31758c943b304b6
전종민 <ho05248@naver.com>
2024-03-24 23:55:51 +0900

    [INIT] Spring Batch Project

--- src/main/java/io/spring/batch/SpringBatchPracticeApplication.java
+++ src/main/java/io/spring/batch/SpringBatchPracticeApplication.java
@@ -2,12 +2,9 @@ package io.spring.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -24,23 +21,19 @@ public class SpringBatchPracticeApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Step step() {
-        return this.stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution,
-                                                ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello, World!");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step1())
                 .build();
     }
 
     @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(step())
+    public Step step1() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("Hello, world!");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
